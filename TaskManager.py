dec974a28fe62841ba09799a267c3a0ab560a679
Samrhan <samuel.bader83@gmail.com>
2021-06-10 10:20:48 +0200

    Initial commit

---
+++ TaskManager.py
@@ -0,0 +1,94 @@
+class Task:
+    def __init__(self, __description, __id):
+        self._description = __description
+        self._status = 0
+        self._id = __id
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, state):
+        self._status = state
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def id(self):
+        return self._id
+
+    def __repr__(self):
+        return f"{self.id} [{' ' if self.status == 0 else 'x'}] {self.description}"
+
+
+class TaskNotFound(Exception):
+    pass
+
+
+class TaskManager:
+    def __init__(self):
+        self._task_list = []
+
+    def add_task(self, description):
+        gen_id = 0
+        if len(self._task_list) != 0:
+            gen_id = max(task.id for task in self._task_list) + 1
+        self._task_list.append(Task(description, gen_id))
+
+    def remove_task(self, __id):
+        task = next((task for task in self._task_list if task.id == int(__id)), None)
+        if task is not None:
+            self._task_list.remove(task)
+        else:
+            raise TaskNotFound
+
+    def change_status(self, __id):
+        task = next((task for task in self._task_list if task.id == int(__id)), None)
+        if task is not None:
+            task.status = not task.status
+        else:
+            raise TaskNotFound
+
+    @property
+    def task_list(self):
+        return self._task_list
+
+    def __repr__(self):
+        return str(self._task_list)
+
+
+class AddAction:
+    def __init__(self, __task_manager, __user_input):
+        action, args = self.parse_command(__user_input)
+        self.triggers = {
+            '+': __task_manager.add_task,
+            '-': __task_manager.remove_task,
+            'x': __task_manager.change_status,
+            'o': __task_manager.change_status
+        }
+        self.triggers[action](args)
+
+    @staticmethod
+    def parse_command(user_input):
+        return user_input[0], user_input[1:].strip()
+
+
+def interaction_loop():
+    user_input = ''
+    task_manager = TaskManager()
+    while user_input != 'q':
+        try:
+            AddAction(task_manager, input())
+            if len(task_manager.task_list) == 0:
+                print("No task yet")
+            for i in task_manager.task_list:
+                print(i)
+        except TaskNotFound:
+            print("Task not found")
+        except ValueError:
+            print("Incorrect Value")
+
+# interaction_loop()
---
+++ test_TaskManager.py
@@ -0,0 +1,58 @@
+from TaskManager import TaskManager
+
+def test_add_a_task():
+    task_manager = TaskManager()
+    task_manager.add_task('Python')
+    assert task_manager.task_list[0].status == 0 and task_manager.task_list[0].description == 'Python'
+
+
+def test_task_good_display():
+    task_manager = TaskManager()
+    task_manager.add_task('Python')
+    assert str(task_manager.task_list[0]) == "0 [ ] Python"
+
+
+def test_generate_good_id():
+    task_manager = TaskManager()
+    for i in range(2):
+        task_manager.add_task(i)
+    for i, task in enumerate(task_manager.task_list):
+        assert task.id == i
+
+
+def test_remove_task():
+    task_manager = TaskManager()
+    for i in range(2):
+        task_manager.add_task(i)
+    list_len = len(task_manager.task_list)
+    task_manager.remove_task(0)
+    assert len(task_manager.task_list) == list_len - 1
+
+
+def test_change_task_status_to_done():
+    task_manager = TaskManager()
+    for i in range(2):
+        task_manager.add_task(i)
+    for i in range(2):
+        task_manager.change_status(i)
+    for i in range(2):
+        assert task_manager.task_list[i].status == 1
+
+
+def test_change_task_status_to_todo():
+    task_manager = TaskManager()
+    for i in range(2):
+        task_manager.add_task(i)
+    for i in range(2):
+        task_manager.change_status(i)
+    for i in range(2):
+        task_manager.change_status(i)
+    for i in range(2):
+        assert task_manager.task_list[i].status == 0
+
+
+def test_good_task_done_display():
+    task_manager = TaskManager()
+    task_manager.add_task('Python')
+    task_manager.change_status(0)
+    assert str(task_manager.task_list[0]) == "0 [x] Python"
